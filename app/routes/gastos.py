a83d9c4ca711604c75e0dcc90096002fec95fda2
Gustavo-Freitas-Dev <gustavoluis2163@gmail.com>
2026-04-06 22:56:48 -0300

    Criando as classes de categoria e subcategoria dentro do arquivo schemas

--- app/routes/gastos.py
+++ app/routes/gastos.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
-from app.schemas.gastos_schemas import CriarGastos
+from app.schemas.gastos_schemas import CriarGastos, AtualizarGastos
 from app.database.db import lista_gastos, contador_gastos
 
-router = APIRouter(prefix='/ganhos', tags=['Gastos'])
+router = APIRouter(prefix='/gastos', tags=['Gastos'])
 
-@router.post('/gastos',
+@router.post('/',
     summary='Cadastra um novo gasto',
     description='Rota para cadastrar um novo gasto ao banco de dados'
 )
@@ -13,7 +13,8 @@ def add_gastos(dado_gasto: CriarGastos):
 
     gasto = {
         'id': contador_gastos,
-        'tipo': dado_gasto.tipo.upper(),
+        'categoria': dado_gasto.categoria.value,
+        'descricao': dado_gasto.descricao.title(),
         'valor': dado_gasto.valor 
     }
 
@@ -22,9 +23,33 @@ def add_gastos(dado_gasto: CriarGastos):
 
     return gasto
 
-@router.get('/gastos',
+@router.get('/',
     summary='Vizualizar os gastos',
     description='Retorna todos os gatos cadastrados no banco de dados'
 )
 def view_gastos():
     return lista_gastos
+
+@router.put('/{id}',
+    summary='Atualizar gastos',
+    description='Rota para atualizar um gasto cadastrado no banco de dados.'
+)
+def update_gastos(id:int, dado:AtualizarGastos):
+    for gasto in lista_gastos:
+        if gasto['id'] == id:
+            gasto['categoria'] = dado.categoria
+            gasto['descricao'] = dado.descricao
+            gasto['valor'] = dado.valor
+            return {'ATUALIZADO': 'Gasto atualizado com sucesso!'}
+    return {'ERRO': 'ID não indentificado.'}
+
+@router.delete('/{id}',
+    summary='Deleta um gasto',
+    description='Rota para deletar um gasto cadastrado no banco de dados.'
+)
+def delete_gasto(id:int):
+    for gasto in lista_gastos:
+        if gasto['id'] == id:
+            lista_gastos.remove(gasto)
+            return {'DELETADO': gasto}
+    return {'ERRO': 'ID não indentificado.'}
--- app/schemas/gastos_schemas.py
+++ app/schemas/gastos_schemas.py
@@ -1,6 +1,34 @@
 from pydantic import BaseModel, Field
+from enum import Enum
+
+class CategoriaGasto(str, Enum):
+    combustivel = "COMBUSTIVEL"
+    manutencao = "MANUTENCAO"
+    operacional = "OPERACIONAL"
+    pessoal = "PESSOAL"
+    outros = "OUTROS"
+
+class SubcategoriaGasto(str, Enum):
+    oleo = "OLEO"
+    pneu = "PNEU"
+    freio = "FREIO"
+    alinhamento = "ALINHAMENTO"
+    revisao = "REVISAO"
+    lavagem = "LAVAGEM"
+    estacionamento = "ESTACIONAMENTO"
+    pedagio = "PEDAGIO"
+    alimentacao = "ALIMENTACAO"
+    internet = "INTERNET"
+    celular = "CELULAR"
+    multa = "MULTA"
+    imprevisto = "IMPREVISTO"
 
 class CriarGastos(BaseModel):
-    tipo: str = Field(..., example='Combustível')
+    categoria: CategoriaGasto
+    descricao: str = Field(..., example='posto ipiranga')
     valor: float = Field(..., example=50.84)
 
+class AtualizarGastos(BaseModel):
+    categoria: CategoriaGasto
+    descricao: str = Field(..., example='Academia')
+    valor: float = Field(..., example=62.85)
